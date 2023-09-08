0e427a0a519433431b7368d27db63dddf88e5ba2
Jin Choi <jin.choi@enjine.com>
2023-09-08 14:15:44 -0700

    Added comments

--- main.py
+++ main.py
@@ -1,24 +1,19 @@
 import datetime
 
-import pandas as pd
 import pytz as pytz
 import yfinance as yf
-
-from model.preprocessors import process_inputs, process_targets
-from model.helpers import train, predict
 from matplotlib import pyplot as plt
 from sklearn.metrics import r2_score, mean_absolute_error
+
+from model.helpers import train, predict
+from model.preprocessors import process_inputs, process_targets
 
 if __name__ == "__main__":
     # Download price histories from Yahoo Finance
     spy = yf.Ticker("SPY")
     price_series = spy.history(period='max')['Close'].dropna()
 
-    # perf_series = price_series
     perf_series = price_series.pct_change().dropna()
-    perf_series.to_pickle('./perf_series.pkl')
-
-    perf_series = pd.read_pickle('./perf_series.pkl')
 
     x_df = process_inputs(perf_series, window_length=10)
     y_series = process_targets(perf_series)
@@ -27,17 +22,21 @@ if __name__ == "__main__":
     common_index = x_df.index.intersection(y_series.index)
     x_df, y_series = x_df.loc[common_index], y_series.loc[common_index]
 
+    # Isolate training data
     training_cutoff = datetime.datetime(2020, 1, 1, tzinfo=pytz.timezone('America/New_York'))
     training_x_series = x_df.loc[x_df.index < training_cutoff]
     training_y_series = y_series.loc[y_series.index < training_cutoff]
 
     trained_model = train(training_x_series, training_y_series)
 
+    # Isolate test data
     test_x_series = x_df.loc[x_df.index >= training_cutoff]
-    forecast_series = predict(trained_model, test_x_series)
     actual_series = y_series.loc[y_series.index >= training_cutoff]
+
+    forecast_series = predict(trained_model, test_x_series)
     results_df = forecast_series.to_frame('Forecast').join(actual_series.to_frame('Actual')).dropna()
 
+    # Evaluate forecasts
     results_df.plot.scatter(x='Actual', y='Forecast')
     plt.show()
     print(f"R Squared: {r2_score(results_df['Actual'], results_df['Forecast']):.4f}, "
--- model/preprocessors.py
+++ model/preprocessors.py
@@ -1,7 +1,15 @@
 import pandas as pd
 
 
-def process_inputs(perf_series: pd.Series, window_length: int):
+def process_inputs(perf_series: pd.Series, window_length: int) -> pd.DataFrame:
+    """
+    Creates sequences consisting of data from across a moving window. For example, given a window length of 10,
+    sequences will span indices 0-9, 1-10, 2-11, etc.
+    :param perf_series: The stock price returns data to extract sequences from.
+    :param window_length: The size of the moving window.
+    :return: Pandas DataFrame where each row contains a sequence, and the index refers to the most recent input date,
+    a.k.a. the reference date.
+    """
     dataframes = []
     for i in range(window_length):
         dataframes.append(perf_series.shift(i).to_frame(f"T - {i}"))
@@ -9,5 +17,11 @@ def process_inputs(perf_series: pd.Series, window_length: int):
     return pd.concat(reversed(dataframes), axis=1).dropna()
 
 
-def process_targets(perf_series: pd.Series):
+def process_targets(perf_series: pd.Series) -> pd.Series:
+    """
+    Creates targets consisting of data 2 days after the reference date (i.e. the most recent input's date)
+    :param perf_series: The stock price returns data to extract targets from.
+    :return: A series where the values consist of returns 2 days after the reference dates, and where the index consists
+    of the reference dates.
+    """
     return perf_series.shift(-2).dropna()
